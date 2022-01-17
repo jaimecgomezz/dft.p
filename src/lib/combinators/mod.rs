192502eb2b5e4c1b346895c7cc8d0636f2268088
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-01-16 22:34:25 -0600

    feat: Add the data_value combinator

--- src/lib/combinators/mod.rs
+++ src/lib/combinators/mod.rs
@@ -1,2 +1,46 @@
 mod literals;
 mod types;
+
+use nom::branch::alt;
+use nom::combinator::map;
+
+use crate::types::{Boolean, DataValue, Float, Integer, Text, VResult};
+use types::*;
+
+pub fn data_value(input: &str) -> VResult<&str, DataValue> {
+    alt((float_value, integer_value, boolean_value, text_value))(input)
+}
+
+fn text_value(input: &str) -> VResult<&str, DataValue> {
+    map(text, |result: Text| DataValue::Text(result))(input)
+}
+
+fn boolean_value(input: &str) -> VResult<&str, DataValue> {
+    map(boolean, |result: Boolean| DataValue::Boolean(result))(input)
+}
+
+fn float_value(input: &str) -> VResult<&str, DataValue> {
+    map(float, |result: Float| DataValue::Float(result))(input)
+}
+
+fn integer_value(input: &str) -> VResult<&str, DataValue> {
+    map(integer, |result: Integer| DataValue::Integer(result))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn data_value_test() {
+        assert_eq!(
+            data_value("text"),
+            Ok(("", DataValue::Text("text".to_string())))
+        );
+        assert_eq!(data_value("true"), Ok(("", DataValue::Boolean(true))));
+        assert_eq!(data_value("false"), Ok(("", DataValue::Boolean(false))));
+        assert_eq!(data_value("1.0"), Ok(("", DataValue::Float(1.0))));
+        assert_eq!(data_value("-1.0"), Ok(("", DataValue::Float(-1.0))));
+        assert_eq!(data_value("1"), Ok(("", DataValue::Integer(1))));
+    }
+}
